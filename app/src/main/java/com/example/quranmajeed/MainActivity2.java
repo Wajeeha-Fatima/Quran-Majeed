0dc4e8551c88695e289242165ba6d753cc794637
Rukhsana Yaqoob <bitf19m0362pucit.edu.pk>
2022-12-03 11:20:38 +0500

    first commit

---
+++ app/src/main/java/com/example/quranmajeed/MainActivity2.java
@@ -0,0 +1,34 @@
+package com.example.quranmajeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        btn = findViewById(R.id.button);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+    }
+
+    private void openActivity3() {
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
+    }
+}
